144850985fd137032335395a487941df2f30528b
vwaskey08-dotcom <vwaskey08@gmail.com>
2026-02-09 10:10:14 -0800

    ldkjassdf

--- src/App.java
+++ src/App.java
@@ -5,7 +5,16 @@ import java.util.List;
 
 public class App {
 
-
+    /**
+     * Creates a button
+     * @param frame
+     * @param buttonName
+     * @param buttonX
+     * @param buttonY
+     * @param buttonW
+     * @param buttonH
+     * @return
+     */
     public static JButton makeButton(JFrame frame, String buttonName, int buttonX, int buttonY, int buttonW, int buttonH)
     {
         JButton button = new JButton(buttonName);
@@ -15,25 +24,85 @@ public class App {
         return button;
     }
 
-    public static void createSecondPage()
+    // int x = 0;
+    // int y = 0;
+
+    // //Aisles a-e
+    // for(int i = 0; i < 5; i ++)
+    //     {
+    //         final String aisle = letter[i]; //look into this
+    //         for(int j = 3; j > -1; j --)
+    //         {
+    //             final String seat = num[j];
+    //             makeButton(frame, num[j] + letter[i], x, y, 50,50).addActionListener(e -> {
+    //                 seats.add(seat +": " + aisle);
+    //             });
+
+    //             x += 50;
+    //         }
+    //         x = 0;
+    //         y += 50;
+            
+    //     }
+
+    public static void createAisle(int startX, int startY, int startIndexAisle, int endIndexAisle, String [] letter, boolean forward)
+    {
+        int x = startX;
+        int y = startY;
+
+        for(int i = startIndexAisle; i < endIndexAisle + 1; i ++)
+        {
+            final String aisle = letter[i];
+            if 
+
+        }
+    }
+
+    public static void createFirstPage(List<String> seats)
+    {
+        JFrame frame = new JFrame();
+        makeButton(frame, "Finish", 240, 200, 75, 50).addActionListener(e ->
+            {
+                frame.setVisible(false);
+                createSecondPage(seats);
+            }
+        );
+
+        frame.setSize(600,290);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+
+    }
+
+    /**
+     * Creates the second page
+     * @param seats
+     */
+    public static void createSecondPage(List<String> seats)
     {
         JFrame frame2 = new JFrame();
-        makeButton(frame2, "Random Seat", 100, 50, 200, 60);
 
-        frame2.setSize(400,200);
+        makeButton(frame2, "Random Seat", 100, 50, 200, 60).addActionListener(e ->
+            {
+                makeButton(frame2, seats.get((int)(Math.random()* (seats.size() - 1 - 0 + 1) + 0)), 100, 100, 200, 60);
+            });
+
+
+        frame2.setSize(400,400);
         frame2.setLayout(null);
         frame2.setVisible(true);
     }
 
     public static void main(String[] args) throws Exception {
 
-        JFrame frame = new JFrame();
+        // JFrame frame = new JFrame();
 
         String [] num = {"1" , "2" , "3" , "4"};
         String [] letter = {"a", "b", "c", "d", "e", "f", "g", "h", "i","j"};
         List<String> seats = new ArrayList<>();
 
-        boolean [] exit = {false};
 
         int x = 0;
         int y = 0;
@@ -41,10 +110,10 @@ public class App {
         //Aisles a-e
         for(int i = 0; i < 5; i ++)
             {
-                final int aisle = i; //look into this
+                final String aisle = letter[i]; //look into this
                 for(int j = 3; j > -1; j --)
                 {
-                    final int seat = j;
+                    final String seat = num[j];
                     makeButton(frame, num[j] + letter[i], x, y, 50,50).addActionListener(e -> {
                         seats.add(seat +": " + aisle);
                     });
@@ -62,14 +131,14 @@ public class App {
         //Aisles f-j
         for(int i = 5 ; i < 10; i++)
             {
-                final int aisle = i;
+                final String aisle = letter[i];
 
                 for(int j = 0; j < 4; j++)
                 {
-                    final int seat = j;
+                    final String seat = num[j];
 
                     makeButton(frame, num[j] + letter[i], x, y, 50,50).addActionListener(e -> {
-                        seats.add(seat+": " + aisle);
+                        seats.add(seat +": " + aisle);
                     });
     
                     x += 50;
@@ -79,22 +148,21 @@ public class App {
             
         }
         
-        makeButton(frame, "Finish", 240, 200, 75, 50).addActionListener(e ->
-            {
-                frame.setVisible(false);
-                createSecondPage();
-            }
-        );
+        // makeButton(frame, "Finish", 240, 200, 75, 50).addActionListener(e ->
+        //     {
+        //         frame.setVisible(false);
+        //         createSecondPage(seats);
+        //     }
+        // );
 
        
     
 
 
-        frame.setSize(600,290);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        // frame.setSize(600,290);
+        // frame.setLayout(null);
+        // frame.setVisible(true);
 
-        // System.out.println(seats);  need to add another button to exit and than test this 
 
         
         
